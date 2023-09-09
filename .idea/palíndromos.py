8acb5542a5eb354145d82bbe112cc190f2da9ed1
andres <anfega3@gmail.com>
2023-09-08 23:16:36 -0500

    Add files

---
+++ .idea/palíndromos.py
@@ -0,0 +1,18 @@
+def es_palindromo(numero):
+
+    numero_str = str(numero)
+
+    #
+    if numero_str == numero_str[::-1]:
+        return True
+    else:
+        return False
+
+
+
+numero = input("Ingrese un numero: ")
+
+if es_palindromo(numero):
+    print(numero, "es palindromo")
+else:
+    print(numero, "no es palindromo")
